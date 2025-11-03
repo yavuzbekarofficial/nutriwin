ef8a587bfa694854144e2b53233d541a01f5f80f
yavuzbekar00 <yavuz.00.bekar@gmail.com>
2025-11-03 13:42:23 +0300

    add google tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,43 +2,62 @@ import MainLayout from "@/layout/MainLayout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MainLayout>
-      <Head>
-        {/*
-          FAVICON İYİLEŞTİRMELERİ
-          Masaüstü/Genel Tarayıcılar (ICO ve 32x32 PNG)
-        */}
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
+    <>
+      {/* Google tag (gtag.js) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-KL8XJM1NJN"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-KL8XJM1NJN', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
 
-        {/*
-          Mobil (Apple Touch Icon)
-          Bu, iPhone ana ekranına eklenmek üzere kullanılır.
-        */}
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
+      <MainLayout>
+        <Head>
+          {/*
+            FAVICON İYİLEŞTİRMELERİ
+            Masaüstü/Genel Tarayıcılar (ICO ve 32x32 PNG)
+          */}
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
 
-        {/*
-          Web Uygulama Manifestosu
-          Android ve Chrome için gereklidir, Google'ın tanınmasını kolaylaştırır.
-        */}
-        <link rel="manifest" href="/site.webmanifest" />
+          {/*
+            Mobil (Apple Touch Icon)
+            Bu, iPhone ana ekranına eklenmek üzere kullanılır.
+          */}
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
 
-        {/* Tarayıcının çevirisini engellemek için meta etiketi */}
-        <meta name="google" content="notranslate" />
-      </Head>
-      <Component {...pageProps} />
-    </MainLayout>
+          {/*
+            Web Uygulama Manifestosu
+            Android ve Chrome için gereklidir, Google'ın tanınmasını kolaylaştırır.
+          */}
+          <link rel="manifest" href="/site.webmanifest" />
+
+          {/* Tarayıcının çevirisini engellemek için meta etiketi */}
+          <meta name="google" content="notranslate" />
+        </Head>
+        <Component {...pageProps} />
+      </MainLayout>
+    </>
   );
 }
